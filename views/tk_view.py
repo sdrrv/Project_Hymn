f5697e553d8897f3ad73c7f5e36f7b594e853d8b
Duarte Elvas <duartecelvas@gmail.com>
2020-09-13 00:41:55 +0100

    Added an icon to the main page

--- views/tk_view.py
+++ views/tk_view.py
@@ -2,6 +2,10 @@ import tkinter
 
 class tk_view:
     def __init__(self):
+        #------------------Seting-the-main-screen-------------------------------
         self.app = tkinter.Tk(screenName="main")
         self.app.geometry("300x200")
         self.app.title("Hymn")
+        self.app.iconbitmap("icons/icon.ico")
+        #-----------------------------------------------------------------------
+
