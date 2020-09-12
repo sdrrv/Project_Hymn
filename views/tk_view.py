4c9cc8898f781acd1317d8b93a6bd92723e467ad
Duarte Elvas <duartecelvas@gmail.com>
2020-09-13 00:52:49 +0100

    Added the grid to the main screen

--- views/tk_view.py
+++ views/tk_view.py
@@ -2,10 +2,14 @@ import tkinter
 
 class tk_view:
     def __init__(self):
-        #------------------Seting-the-main-screen-------------------------------
+        #------------------Creating-the-main-screen-----------------------------
         self.app = tkinter.Tk(screenName="main") #Making the main root
         self.app.geometry("300x200") # Setting the size of the main window
         self.app.title("Hymn") 
         self.app.iconbitmap("icons/icon.ico") #Setting the main left top icon
-        #-----------------------------------------------------------------------
-
+        #-----------------Populating-the-main-screen----------------------------
+        #.........-Grid..................................
+        self.music_grid= tkinter.Listbox(master=self.app)
+        self.music_grid.grid(row=0,column=0)
+        #..........Buttons...............................
+        #..........Entry.................................
