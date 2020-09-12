971677dea8c3f93c36b831c861ea2041a1d003a4
Duarte Elvas <duartecelvas@gmail.com>
2020-09-13 00:45:02 +0100

    Added some comments to explain some classes

--- models/chrome_web_driver.py
+++ models/chrome_web_driver.py
@@ -2,13 +2,13 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome import Options
 
 class chr_driver:
-    def __init__(self,headless=False):
-        self.driver = Chrome()
+    def __init__(self,headless=False): # not setting headless to True will put it to default, witch is False.
+        self.driver = Chrome() #Setting the main driver
         self.opts= Options()
-        self.opts.headless= headless
+        self.opts.headless= headless # setting the headless mode to the value inputed above
     
-    def get_driver(self):
+    def get_driver(self): #get the main driver
         return self.driver
     
-    def close(self):
+    def close(self): #close the driver
         self.get_driver().close()
--- views/tk_view.py
+++ views/tk_view.py
@@ -3,9 +3,9 @@ import tkinter
 class tk_view:
     def __init__(self):
         #------------------Seting-the-main-screen-------------------------------
-        self.app = tkinter.Tk(screenName="main")
-        self.app.geometry("300x200")
-        self.app.title("Hymn")
-        self.app.iconbitmap("icons/icon.ico")
+        self.app = tkinter.Tk(screenName="main") #Making the main root
+        self.app.geometry("300x200") # Setting the size of the main window
+        self.app.title("Hymn") 
+        self.app.iconbitmap("icons/icon.ico") #Setting the main left top icon
         #-----------------------------------------------------------------------
 
